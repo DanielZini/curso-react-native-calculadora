bd52c0df8dc0d639e81432db40d28f6ff26bba5f
daniel.zini@hotmail.com <daniel.zini@hotmail.com>
2019-07-02 23:03:58 -0300

    Implementação do Layout

--- App.js
+++ App.js
@@ -1,49 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import Button from "./src/components/Button";
+import Display from "./src/components/Display";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+    state = {
+        displayValue: "0"
+    };
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    addDigit = n => {
+        this.setState({ displayValue: n });
+    };
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    clearMemory = () => {
+        this.setState({ displayValue: "0" });
+    };
+
+    setOperation = operation => {};
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Display value={this.state.displayValue} />
+                <View style={styles.buttons}>
+                    <Button label="AC" onClick={this.clearMemory} triple />
+                    <Button label="/" onClick={this.setOperation} operation />
+                    <Button label="7" onClick={this.addDigit} />
+                    <Button label="8" onClick={this.addDigit} />
+                    <Button label="9" onClick={this.addDigit} />
+                    <Button label="*" onClick={this.setOperation} operation />
+                    <Button label="4" onClick={this.addDigit} />
+                    <Button label="5" onClick={this.addDigit} />
+                    <Button label="6" onClick={this.addDigit} />
+                    <Button label="-" onClick={this.setOperation} operation />
+                    <Button label="1" onClick={this.addDigit} />
+                    <Button label="2" onClick={this.addDigit} />
+                    <Button label="3" onClick={this.addDigit} />
+                    <Button label="+" onClick={this.setOperation} operation />
+                    <Button label="0" onClick={this.addDigit} double />
+                    <Button label="." onClick={this.setOperation} />
+                    <Button label="=" onClick={this.setOperation} operation />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1
+    },
+    buttons: {
+        flexDirection: "row",
+        flexWrap: "wrap"
+    }
 });
